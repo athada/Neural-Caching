f5f69e91558ce698f7aba6f1fee741446a015de9
athada <athada@griddynamics.com>
2025-02-10 15:54:30 +0530

    Deleting Duplicate Files

--- Benchmark/TF-Mac-Req.sh
+++
@@ -1,4 +0,0 @@
-brew install libjpeg
-conda install -c apple tensorflow-deps
-python -m pip install tensorflow-macos tensorflow-metal tensorflow-datasets tensorboard tensorboard_plugin_profile
-conda install -y matplotlib pandas jupyterlab
